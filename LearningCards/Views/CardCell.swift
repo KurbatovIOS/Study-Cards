6bff034175ead2ca89ee1329cf5b7c043b69d77d
KurbatovIOS <akurbatov.work@gmail.com>
2022-10-07 22:46:51 +0300

    added button to mark learned cards

--- LearningCards/Views/CardCell.swift
+++ LearningCards/Views/CardCell.swift
@@ -10,10 +10,19 @@ import UIKit
 class CardCell: UITableViewCell {
 
     @IBOutlet weak var cardFrontLabel: UILabel!
+    @IBOutlet weak var statusButton: UIButton!
+    var isLearned = false
     
     func setUpFrontLable(frontLabel: String) {
         
         cardFrontLabel.text = frontLabel
     }
-
+    
+    
+    @IBAction func statusButtonClicked(_ sender: Any) {
+        
+        isLearned.toggle()
+        statusButton.tintColor = isLearned ? .systemGreen : .systemGray2
+        
+    }
 }
